902b4507cfb3a44ba13b29c250d71bff205ff66a
Amukelani Mkhondelela <amukelanimkhondelela@gmail.com>
2023-01-31 20:43:52 +0200

    Dice roll fully working with graphic

--- diceroll.py
+++ diceroll.py
@@ -1,5 +1,53 @@
 import random
 
+DICE_ART = {
+    1: (
+        "┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘",
+    ),
+    2: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    3: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    4: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    5: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    6: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+}
+DIE_HEIGHT = len(DICE_ART[1])
+DIE_WIDTH = len(DICE_ART[1][0])
+DIE_FACE_SEPARATOR = " "
+
 def get_input():
     input_dice_num = input("How many die to roll? [1-6] ")
     return input_dice_num
@@ -28,3 +76,51 @@ def roll_dice(num_dice):
         roll = random.randint(1, 6)
         roll_results.append(roll)
     return roll_results
+
+def generate_dice_faces_diagram(dice_values):
+    """Return an ASCII diagram of dice faces from `dice_values`.
+
+    The string returned contains an ASCII representation of each die.
+    For example, if `dice_values = [4, 1, 3, 2]` then the string
+    returned looks like this:
+
+    ~~~~~~~~~~~~~~~~~~~ RESULTS ~~~~~~~~~~~~~~~~~~~
+    ┌─────────┐ ┌─────────┐ ┌─────────┐ ┌─────────┐
+    │  ●   ●  │ │         │ │  ●      │ │  ●      │
+    │         │ │    ●    │ │    ●    │ │         │
+    │  ●   ●  │ │         │ │      ●  │ │      ●  │
+    └─────────┘ └─────────┘ └─────────┘ └─────────┘
+    """
+    dice_faces = _get_dice_faces(dice_values)
+    dice_faces_rows = _generate_dice_faces_rows(dice_faces)
+
+    # Generate header with the word "RESULTS" centered
+    width = len(dice_faces_rows[0])
+    diagram_header = " RESULTS ".center(width, "~")
+
+    dice_faces_diagram = "\n".join([diagram_header] + dice_faces_rows)
+    return dice_faces_diagram
+
+def _get_dice_faces(dice_values):
+    dice_faces = []
+    for value in dice_values:
+        dice_faces.append(DICE_ART[value])
+    return dice_faces
+
+def _generate_dice_faces_rows(dice_faces):
+    dice_faces_rows = []
+    for row_idx in range(DIE_HEIGHT):
+        row_components = []
+        for die in dice_faces:
+            row_components.append(die[row_idx])
+        row_string = DIE_FACE_SEPARATOR.join(row_components)
+        dice_faces_rows.append(row_string)
+    return dice_faces_rows
+
+if __name__ == "__main__":
+    input = get_input()
+    num_dice = parse_input(input)
+    results = roll_dice(num_dice)
+    dice_diagram = generate_dice_faces_diagram(results)
+    print(f"\n{dice_diagram}")
+    
